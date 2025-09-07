fcb955d0fa1aa3cc3ed3f0a5f119b4eeecbeb407
Maximus9393 <turecak92@gmail.com>
2025-09-07 16:56:27 +0000

    chore: stop tracking backend dist and logs

--- smart-market-finder-1/backend/dist/models/prismaClient.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const globalForPrisma = global;
-const prisma = globalForPrisma.__prismaClient || new client_1.PrismaClient();
-if (process.env.NODE_ENV === 'development')
-    globalForPrisma.__prismaClient = prisma;
-exports.default = prisma;
